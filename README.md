dd9b0d0388a6b1779a8c4576f9122bb280821cbb
John Matter <matter@virginia.edu>
2020-01-24 10:43:52 -0500

    Update distances; add readme; add raster suggestions

---
+++ README.md
@@ -0,0 +1,48 @@
+### Projecting beam width
+We want to make sure the beam width at the target is small enough that
+the halo doesn't hit the target cell wall. We do this by projecting the
+width to the target, and choosing an appropriate raster size for that width.
+
+As in Simona's entry [1] using distances Dave Gaskell sent me [2], we can
+project the beam width from each the harps to the target as follows:
+
+width at target (using 3H07X) = width(3H07X) + 1.55 * [width(3H07B) - width(3H07A)]/1.41
+width at target (using 3H07A) = width(3H07A) + 2.95 * [width(3H07B) - width(3H07A)]/1.41
+
+### Choosing a raster diameter
+If we have a wide beam, we need the raster to be smaller.
+If we have a narrow beam, we can accept a wider raster.
+
+The rule of thumb we established on Jan 23rd, 2020 is as follows:
+| Beam width at target (mm) | Raster diameter (mm)  |
+| ------------------------- | --------------------- |
+| < 0.300                   | 5.0                   |
+| 0.300 to 0.400            | 4.5                   |
+| 0.400 to 0.500            | 4.0                   |
+| > 0.500                   | Request smaller width |
+
+### Note regarding x width
+Jay Benesch says [3]  there's significant background in the first x wires
+of the harps. So we should use the second x wire, peak 3 in the harp fitter
+tool (circled in light blue below). For y, use peak 2 (circled in red below).
+The y width has tended to be pretty narrow, so we're mainly concerned with x.
+
+![harps](both_harps.png)
+
+### Example usage
+```~/a1n_project_beam_width$ ./project_beam_to_target.py
+Width at IHA3H07A in mm: 0.3267
+Width at IHA3H07B in mm: 0.3807
+
+Beam width projected to target (both harps should match)
+From IHA3H07A: 0.4401
+From IHA3H07B: 0.4401
+
+Raster insructions:
+Request a raster diameter of 4.00 mm
+```
+
+### [citation needed]
+[1] https://logbooks.jlab.org/entry/3768482
+[2] https://hallcweb.jlab.org/elogs/Commissioning+Experiments+Analysis/34
+[3] https://logbooks.jlab.org/entry/3751286
--- project_beam_to_target.py
+++ project_beam_to_target.py
@@ -1,39 +1,48 @@
 #!/usr/local/bin/python3
 import sys
 
-# Per Simona's entry [1] using distances compiled by Daniel Moser [2], we can
-# project the beam width from each the harps to the target as follows:
-#
-# width_x at target (using 3H07X) = width_x(3H07X) + 1.55 * [width_x(3H07B) - width_x(3H07A)]/1.91
-# width_x at target (using 3H07A) = width_x(3H07A) + 3.46 * [width_x(3H07B) - width_x(3H07A)]/1.91
-#
-# [1] https://logbooks.jlab.org/entry/3768482
-# [2] https://logbooks.jlab.org/entry/3764458
+IHA3H07Asigmax = input("Width at IHA3H07A in mm: ")
+IHA3H07Bsigmax = input("Width at IHA3H07B in mm: ")
 
-if (len(sys.argv)!=3):
-    print("Usage: projectBeamWidthToTarget.py IHA3H07Awidth IHA3H07Bwidth ")
-    sys.exit(1)
-
-# In millimeters
 try:
-    IHA3H07Asigmax = float(sys.argv[1])
-    IHA3H07Bsigmax = float(sys.argv[2])
+    IHA3H07Asigmax = float(IHA3H07Asigmax)
+    IHA3H07Bsigmax = float(IHA3H07Bsigmax)
 except ValueError:
-    raise("Input widths should be a float")
-    
+    raise("Input widths should be numbers")
 
-# In meters
-distanceBetweenHarps = 1.91
-distanceFromBtoTarget = 1.55
-distanceFromAtoTarget = 3.46
-
+# Calculate difference for slope
 deltaSigmax = (IHA3H07Bsigmax-IHA3H07Asigmax)
 
+# In meters
+distanceFromAtoTarget = 2.9521
+distanceFromBtoTarget = 1.5468
+distanceBetweenHarps = distanceFromAtoTarget - distanceFromBtoTarget
+
+# Project to target
 targetProjectedFromA = IHA3H07Asigmax + distanceFromAtoTarget * (deltaSigmax/distanceBetweenHarps)
 targetProjectedFromB = IHA3H07Bsigmax + distanceFromBtoTarget * (deltaSigmax/distanceBetweenHarps)
 
+# Determine appropriate rasterDiameter size
+goodWidth = True
+widthInMicrons = int(targetProjectedFromA*1e3)
+if widthInMicrons > 500:
+    goodWidth = False
+    rasterDiameter = -1
+if widthInMicrons in range(400,500):
+    rasterDiameter = 4.0
+if widthInMicrons in range(300,400):
+    rasterDiameter = 4.5
+if widthInMicrons < 300:
+    rasterDiameter = 5.0
+    
+# Display results
 print("")
 print("Beam width projected to target (both harps should match)")
-print("from IHA3H07A: %.4f mm" % targetProjectedFromA)
-print("from IHA3H07B: %.4f mm" % targetProjectedFromB)
+print("From IHA3H07A: %.4f" % targetProjectedFromA)
+print("From IHA3H07B: %.4f" % targetProjectedFromB)
 print("")
+print("Raster insructions: ")
+if goodWidth:
+    print("Request a raster diameter of %0.2f mm" % rasterDiameter)
+else:
+    print("Request a smaller width from MCC.")
